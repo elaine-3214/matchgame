b208ab84a02c07e6e8eb028f5d3f8f2481637b62
Elaine <elainez13@nycstudents.net>
2023-10-25 10:34:33 -0400

    Initial commit

---
+++ matchGame.js
@@ -0,0 +1,7 @@
+var message = docoument.getElementById("p");
+
+function setName(){
+    var userName = form.userName.value;
+    message.textContent = "Hi. " + userName + ". You may now press the button to start the game!";
+
+}
