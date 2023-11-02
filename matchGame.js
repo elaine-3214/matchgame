ce631e1d5356db7ab668112be3918d819af1c280
Elaine <elainez13@nycstudents.net>
2023-11-02 10:47:17 -0400

    draft 3

--- matchGame.js
+++ matchGame.js
@@ -17,8 +17,8 @@ function startgame(){
     var card8 = document.getElementById(8);
     var card9 = document.getElementById(9);
     var card10 = document.getElementById(10);
-    var card11 = document.getElementById(11);
-    var card12 = document.getElementById(12);
+    var card12 = document.getElementById(11);
+    var card11 = document.getElementById(12);
     var card13 = document.getElementById(13);
     var card14 = document.getElementById(14);
     var card15 = document.getElementById(15);
@@ -29,5 +29,32 @@ function startgame(){
         var randIndx = Math.random()*numsAvailable.length;
         cards[i] = numsAvailable[randIndx];
         numsAvailable = numsAvailable.splice(randIndx, 1);
+    }
+    for (let j = 0; j<17; j++){
+        if (cards[j]==1){
+            card1.src = "matchgamepics/funny1.jpg";
+        }
+        if (cards[j]==2){
+            card2.src = "matchgamepics/funny2.jpg";
+        }
+        if (cards[j]==3){
+            card3.src = "matchgamepics/funny3.webp";
+        }
+        if (cards[j]==4){
+            card4.src = "matchgamepics/funny4.webp";
+        }
+        if (cards[j]==5){
+            card5.src = "matchgamepics/funny5.jpg";
+        }
+        if (cards[j]==6){
+            card6.src = "matchgamepics/funny6.webp";
+        }
+        if (cards[j]==7){
+            card7.src = "matchgamepics/funny7.jpg";
+        }
+        if (cards[j]==8){
+            card8.src = "matchgamepics/funny8.png";
+        }
+
     }
 }
