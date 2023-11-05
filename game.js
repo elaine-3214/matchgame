0161dc62c3adb1ef926f53294ae3e065657a88c2
elaine <elainez13@nycstudents.net>
2023-11-05 14:58:57 -0500

    Co-authored-by: wberri <wberri@users.noreply.github.com>

--- game.js
+++ game.js
@@ -1,76 +1,115 @@
-var button1 = document.getElementById("1b")
-button1.addEventListener("click", flip());
-var button2 = document.getElementById("2b")
-button2.addEventListener("click", flip());
-var button3 = document.getElementById("3b")
-button3.addEventListener("click", flip());
-var button4 = document.getElementById("4b")
-button4.addEventListener("click", flip());
-var button5 = document.getElementById("5b")
-button5.addEventListener("click", flip());
-var button6 = document.getElementById("6b")
-button6.addEventListener("click", flip());
-var button7 = document.getElementById("7b")
-button7.addEventListener("click", flip());
-var button8 = document.getElementById("8b")
-button8.addEventListener("click", flip());
-var button9 = document.getElementById("9b")
-button9.addEventListener("click", flip());
-var button10 = document.getElementById("10b")
-button10.addEventListener("click", flip());
-var button11 = document.getElementById("11b")
-button11.addEventListener("click", flip());
-var button12 = document.getElementById("12b")
-button12.addEventListener("click", flip());
-var button13 = document.getElementById("13b")
-button13.addEventListener("click", flip());
-var button14 = document.getElementById("14b")
-button14.addEventListener("click", flip());
-var button15 = document.getElementById("15b")
-button15.addEventListener("click", flip());
-var button16 = document.getElementById("16b")
-button16.addEventListener("click", flip());
-
-
-function flip(){
-}
-
-var cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-
+var clicks =0;
+var upTo2= 0;
+var image1 = document.getElementById(1);
+var image2 = document.getElementById(2);
+var image3 = document.getElementById(3);
+var image4 = document.getElementById(4);
+var image5 = document.getElementById(5);
+var image6 = document.getElementById(6);
+var image7 = document.getElementById(7);
+var image8 = document.getElementById(8);
+var image9 = document.getElementById(9);
+var image10 = document.getElementById(10);
+var image11 = document.getElementById(11);
+var image12 = document.getElementById(12);
+var image13 = document.getElementById(13);
+var image14 = document.getElementById(14);
+var image15 = document.getElementById(15);
+var image16 = document.getElementById(16);
+var listOfImages = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16];
 function startgame() {
     var numsAvailable = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+    var cardsScramble = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
     for (let i = 0; i < 16; i++) {
         var randIndx = Math.floor(Math.random() * numsAvailable.length);
-        cards[i] = numsAvailable[randIndx];
+        cardsScramble[i] = numsAvailable[randIndx];
         numsAvailable.splice(randIndx, 1);
     }
-    for (let j = 0; j<16; j++){
-        var currCard = document.getElementById((j + 1));
-        if (cards[j]==1){
-            currCard.src = "gamepics/funny1.jpg";
-        }
-        if (cards[j]==2){
-            currCard.src = "gamepics/funny2.jpg";
-        }
-        if (cards[j]==3){
-            currCard.src = "gamepics/funny3.jpg";
-        }
-        if (cards[j]==4){
-            currCard.src = "gamepics/funny4.jpg";
-        }
-        if (cards[j]==5){
-            currCard.src = "gamepics/funny5.jpg";
-        }
-        if (cards[j]==6){
-            currCard.src = "gamepics/funny6.jpg";
-        }
-        if (cards[j]==7){
-            currCard.src = "gamepics/funny7.jpg";
-        }
-        if (cards[j]==8){
-            currCard.src = "gamepics/funny8.jpg";
-        }
-        
+    cards = cardsScramble;
+    image1.addEventListener("click", () => showCard(1));
+    image2.addEventListener("click", () => showCard(2));
+    image3.addEventListener("click", () => showCard(3));
+    image4.addEventListener("click", () => showCard(4));
+    image5.addEventListener("click", () => showCard(5));
+    image6.addEventListener("click", () => showCard(6));
+    image7.addEventListener("click", () => showCard(7));
+    image8.addEventListener("click", () => showCard(8));
+    image9.addEventListener("click", () => showCard(9));
+    image10.addEventListener("click", () => showCard(10));
+    image11.addEventListener("click", () => showCard(11));
+    image12.addEventListener("click", () => showCard(12));
+    image13.addEventListener("click", () => showCard(13));
+    image14.addEventListener("click", () => showCard(14));
+    image15.addEventListener("click", () => showCard(15));
+    image16.addEventListener("click", () => showCard(16));
+}
+
+function checkNumClicks() {
+    if (upTo2 >= 2) {
+        checkIfSame();
+        setTimeout(() => {
+            hideAll();
+            upTo2 = 0;
+        }, 1000); // 1000 milliseconds (1 seconds)
     }
+}
+
+function checkIfSame(){
+    let potentialMatch = [];
+    let indexList = [];
+    for (let i=0; i<listOfImages.length; i++){
+        if (listOfImages[i].src !== "card.png") {
+            potentialMatch.push(listOfImages[i]);
+            indexList.push(i);
+        }
+    }
+    if((potentialMatch[0].src===potentialMatch[1].src)){
+        listOfImages.splice(indexList[0], 1);
+        listOfImages.splice((indexList[1])-1, 1);
+        potentialMatch = [];
+    }
+}
+
+
+function hideAll(){
+    for (let i = 0; i<listOfImages.length; i++){
+        listOfImages[i].src = "card.png";
+    }
+}
+
+function showCard(imageID){
+    upTo2++;
+    clicks++;
+    checkNumClicks();
+    var currCard = document.getElementById(imageID);
+    if (cards[imageID-1]==1){
+        currCard.src = "gamepics/funny1.jpg";
+    }
+    if (cards[imageID-1]==2){
+        currCard.src = "gamepics/funny2.jpg";
+    }
+    if (cards[imageID-1]==3){
+        currCard.src = "gamepics/funny3.jpg";
+    }
+    if (cards[imageID-1]==4){
+        currCard.src = "gamepics/funny4.jpg";
+    }
+    if (cards[imageID-1]==5){
+        currCard.src = "gamepics/funny5.jpg";
+    }
+    if (cards[imageID-1]==6){
+        currCard.src = "gamepics/funny6.jpg";
+    }
+    if (cards[imageID-1]==7){
+        currCard.src = "gamepics/funny7.jpg";
+    }
+    if (cards[imageID-1]==8){
+        currCard.src = "gamepics/funny8.jpg";
+    }
+}
+
+function updateClicks() {
+    var text = document.getElementById("Clicks");
+    text.innerHTML = clicks;
 }
