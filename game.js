be8ada1fde9a865c3593202e58ac1a1c2f7e05fa
Elaine <elainez13@nycstudents.net>
2023-11-06 11:03:39 -0500

    draft

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var clicks =0;
+var cards = null;
 var upTo2= 0;
 var image1 = document.getElementById(1);
 var image2 = document.getElementById(2);
@@ -18,6 +19,7 @@ var image15 = document.getElementById(15);
 var image16 = document.getElementById(16);
 var listOfImages = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16];
 function startgame() {
+    console.log("test pnt1"); //TEST
     var numsAvailable = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
     var cardsScramble = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
@@ -56,32 +58,52 @@ function checkNumClicks() {
 }
 
 function checkIfSame(){
+    console.log("test pnt3"); //TEST
     let potentialMatch = [];
     let indexList = [];
     for (let i=0; i<listOfImages.length; i++){
-        if (listOfImages[i].src !== "card.png") {
+        console.log(listOfImages[i].src);//splice listOfImages[i].src into only the last part
+        //getLastSegment(path);
+        if (listOfImages[i].src !== "http://127.0.0.1:5500/matchgame/card.png") {
             potentialMatch.push(listOfImages[i]);
             indexList.push(i);
         }
     }
-    if((potentialMatch[0].src===potentialMatch[1].src)){
+    console.log(potentialMatch.length);
+    if((potentialMatch.length===2 && potentialMatch[0].src===potentialMatch[1].src)){
+        console.log(potentialMatch[0]);
+    console.log(potentialMatch[1]);
         listOfImages.splice(indexList[0], 1);
         listOfImages.splice((indexList[1])-1, 1);
         potentialMatch = [];
     }
+    potentialMatch =[];
+    upTo2 = 0;
+}
+
+function getLastSegment(path){
+    // lastIndx = null;
+    // currIndex = path.length-1;
+    // while (lastIndx == null){
+    //     if (path.substring(currIndex, ))
+    // }
 }
 
 
 function hideAll(){
+    console.log("test pnt4"); //TEST
     for (let i = 0; i<listOfImages.length; i++){
         listOfImages[i].src = "card.png";
     }
 }
 
 function showCard(imageID){
+    console.log("test pnt2"); //TEST
     upTo2++;
+    console.log(upTo2);
     clicks++;
     checkNumClicks();
+    updateClicks();
     var currCard = document.getElementById(imageID);
     if (cards[imageID-1]==1){
         currCard.src = "gamepics/funny1.jpg";
