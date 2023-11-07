4fa854204a3e480f0ce3afee52dc7732007b2817
elaine <elainez13@nycstudents.net>
2023-11-07 14:57:36 -0500

    final 5

--- matchGame.js
+++
@@ -1,7 +0,0 @@
-var message = document.getElementById("p");
-
-function setName(){
-    var userName = form.userName.value;
-    message.textContent = "Hi. " + userName + ". You may now press the button to start the game!";
-
-}
